9777baabcd24c7c79a3ef25deb5df2d4081e1367
phillips <qwertysonata@gmail.com>
2025-04-28 18:28:22 +0530

    ok

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+  compiler: {
+    removeConsole: false,
+  },
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  }
+}
+
+module.exports = nextConfig 
